5cda4852ca68d70d886e70c310292c33218c0c65
chaitu0599 <57953120+chaitu0599@users.noreply.github.com>
2020-07-01 23:33:13 +0530

    Updates

---
+++ precache-manifest.042b468d0903bb5a3a88bdd180443c8c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211dec0f76fdb1271650b0e348b6f518",
+    "url": "/Chaitus-portfolio/index.html"
+  },
+  {
+    "revision": "b2a03f32a634936d35b8",
+    "url": "/Chaitus-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6b7035de2f375e984e72",
+    "url": "/Chaitus-portfolio/static/css/main.85afebb6.chunk.css"
+  },
+  {
+    "revision": "b2a03f32a634936d35b8",
+    "url": "/Chaitus-portfolio/static/js/2.1cb392d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Chaitus-portfolio/static/js/2.1cb392d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b7035de2f375e984e72",
+    "url": "/Chaitus-portfolio/static/js/main.58b3a049.chunk.js"
+  },
+  {
+    "revision": "e7ad22c39fe4e2a4211b",
+    "url": "/Chaitus-portfolio/static/js/runtime-main.bb01af61.js"
+  },
+  {
+    "revision": "54f2902ac3e7374dc2324eff0ecb8312",
+    "url": "/Chaitus-portfolio/static/media/Aeroplane.54f2902a.jpg"
+  },
+  {
+    "revision": "a5196796d4380d43a349dc7c0a91dc4f",
+    "url": "/Chaitus-portfolio/static/media/EducAR.a5196796.jpg"
+  },
+  {
+    "revision": "ef635b3ec94ee9f40e33e7522943422b",
+    "url": "/Chaitus-portfolio/static/media/blog.ef635b3e.jpg"
+  }
+]);
